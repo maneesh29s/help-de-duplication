8a5861e988519ee7a533d231af4a6bd2464216e1
maneeshsutar <sutarmaneesh00@gmail.com>
2022-02-22 23:02:16 +0530

    Add: hashcode for value x and y

--- src/main/java/com/dhruvk/point/Point.java
+++ src/main/java/com/dhruvk/point/Point.java
@@ -1,5 +1,7 @@
 package com.dhruvk.point;
 
+import java.util.Objects;
+
 public class Point {
     private final int x;
     private final int y;
@@ -12,10 +14,16 @@ public class Point {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
+
         if (o == null || getClass() != o.getClass()) return false;
+
         Point point = (Point) o;
         return x == point.x &&
                 y == point.y;
     }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
 }
